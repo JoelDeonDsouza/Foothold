a9db804bef5f5e2ce08c2f2d888f6a4d949c72da
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-01-09 03:08:08 +0100

    few Changes in server

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import ApolloProvider from "./apolloprovider";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
