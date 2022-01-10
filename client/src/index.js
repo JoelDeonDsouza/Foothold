53a7eed4ded54086c4dc9355369f4abc999c8843
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-01-10 10:17:35 +0100

    Tag

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import ApolloProvider from "./apolloprovider";
-
+//render not-acomplished
 ReactDOM.render(ApolloProvider, document.getElementById("root"));
 
 reportWebVitals();
