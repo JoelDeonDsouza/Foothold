d235b1b953d5f5509c5810480ad678ac5e8c4c68
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-01-11 03:12:28 +0100

    Resulted with two UnhandledSchemeError, yet to be solve

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+
+import MenuBar from "./components/MenuBar";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
   return (
-    <div>
-      <h1>Footholds 1</h1>
-    </div>
+    <Router>
+      <MenuBar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Router>
   );
 }
 
